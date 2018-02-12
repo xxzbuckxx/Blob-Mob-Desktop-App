82057a83c3fd2fcbcafd1b35ef1043e88544570e
Zack <ztraczyk.coding@gmail.com>
2018-02-11 19:54:38 -0600

    Game working import and menu layout added

--- index.js
+++ index.js
@@ -2,18 +2,51 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
 
 //Listen for app to be ready
 app.on('ready', function(){
     //Create new window
-    mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({width: 900, height: 800, title: 'Blob Mob'});
     //Load html into window
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'mainWindow.html'),
+        pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
     }));
+    
+    //Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    //Insert menu
+    Menu.setApplicationMenu(mainMenu);
 });
+
+//Create menu template
+const mainMenuTemplate = [
+    {
+        label:'options',
+        submenu: [
+            {
+                label: 'Mute'
+            },
+            {
+                label: 'Mute music'
+            },
+            {
+                label: 'Mute effects'
+            },
+            {
+                label: 'Pause'
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
